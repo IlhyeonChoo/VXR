4bf980bf0a4c4f9baaeff751d2c362650b50c891
Ilhyeon Chu <168050490+ilhyeonchu@users.noreply.github.com>
2026-03-31 01:41:34 +0900

    video 10

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+
+    Rigidbody rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+
+        rb.linearVelocity = transform.forward * speed;
+
+        Destroy(this.gameObject, 3f); // 3초 뒤에 이 총알 오브젝트를 제거하겠다.
+    }
+
+    void Update()
+    {
+        
+    }
+}
